61bd2c58c406b0892a7b376db3ca1b7b360f6a9c
Hariprasanth-E <hariprasanth208989@gmail.com>
2024-07-18 21:22:12 +0530

    Upload project files.

---
+++ Theatre.java
@@ -0,0 +1,16 @@
+package TheatreBooking;
+
+public class Theatre { 
+
+	 String movieName = "LEO";
+	  
+	/*  
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	*/
+	
+}
